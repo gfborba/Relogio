aefed8895360e63150951cfd2f529d660dbfc785
Guilherme Borba <guifborba@hotmail.com>
2022-06-01 18:16:50 -0300

    Relógio

---
+++ exercicio/script.js
@@ -0,0 +1,27 @@
+
+function carregar() {
+    var msg= document.getElementById('msg') 
+    var img= window.document.getElementById('imagem')
+    var edu= window.document.getElementById ('edu')
+    var datah = new Date()
+     var datam = new Date()
+    var hora = datah.getHours()
+    var min = datam.getMinutes()
+    msg.innerHTML = (`Agora são <strong>${hora} horas e ${min} minutos.</strong>`)
+        if(hora>0 && hora<6) {img.src='madrugada.png' 
+        document.body.style.background ='#011e30'
+         edu.innerText = ('Boa Madrugada! Não durma muito tarde.')
+        } else if (hora>=6 && hora<=12) {img.src='manha.png' 
+        document.body.style.background ='#feec8a'
+         edu.innerText = ('Bom Dia! Não desanime, uma coisa de cada vez.')
+        } else if (hora>=13 && hora<18){img.src='tarde.png'
+        document.body.style.background ='#8c9dad'
+         edu.innerText =('Boa Tarde! Não se esqueça do cafézinho.')
+        } else {img.src='noite.png'
+        document.body.style.background ='#534266'
+        edu.innerText = ('Boa Noite! Você conseguiu, hora de descansar.')
+        
+        
+    }
+    
+}
